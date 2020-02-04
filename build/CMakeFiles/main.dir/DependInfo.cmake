f9b7f6f7090032e8f260a255c2497ac1033678ae
Athanasios Martsopoulos <amartsop@gmail.com>
2020-02-04 15:32:16 +0000

    first commit

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/thanos/Desktop/Development/Graphics4/src/stb_image.c" "/home/thanos/Desktop/Development/Graphics4/build/CMakeFiles/main.dir/src/stb_image.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".././libs/glfw-3.1.2/include"
+  ".././libs/glm-0.9.7.1"
+  ".././libs/glew-1.13.0/include"
+  ".././include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/thanos/Desktop/Development/Graphics4/main.cpp" "/home/thanos/Desktop/Development/Graphics4/build/CMakeFiles/main.dir/main.cpp.o"
+  "/home/thanos/Desktop/Development/Graphics4/src/camera.cpp" "/home/thanos/Desktop/Development/Graphics4/build/CMakeFiles/main.dir/src/camera.cpp.o"
+  "/home/thanos/Desktop/Development/Graphics4/src/display.cpp" "/home/thanos/Desktop/Development/Graphics4/build/CMakeFiles/main.dir/src/display.cpp.o"
+  "/home/thanos/Desktop/Development/Graphics4/src/mesh.cpp" "/home/thanos/Desktop/Development/Graphics4/build/CMakeFiles/main.dir/src/mesh.cpp.o"
+  "/home/thanos/Desktop/Development/Graphics4/src/obj_loader.cpp" "/home/thanos/Desktop/Development/Graphics4/build/CMakeFiles/main.dir/src/obj_loader.cpp.o"
+  "/home/thanos/Desktop/Development/Graphics4/src/shader.cpp" "/home/thanos/Desktop/Development/Graphics4/build/CMakeFiles/main.dir/src/shader.cpp.o"
+  "/home/thanos/Desktop/Development/Graphics4/src/texture.cpp" "/home/thanos/Desktop/Development/Graphics4/build/CMakeFiles/main.dir/src/texture.cpp.o"
+  "/home/thanos/Desktop/Development/Graphics4/src/transform.cpp" "/home/thanos/Desktop/Development/Graphics4/build/CMakeFiles/main.dir/src/transform.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././libs/glfw-3.1.2/include"
+  ".././libs/glm-0.9.7.1"
+  ".././libs/glew-1.13.0/include"
+  ".././include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/thanos/Desktop/Development/Graphics4/build/libs/glfw-3.1.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/thanos/Desktop/Development/Graphics4/build/libs/CMakeFiles/GLEW_1130.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
